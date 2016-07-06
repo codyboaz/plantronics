f23cf450e131be2f9c12b8fbe252ac9822b596da
Cody Boaz <codyryanboaz@yahoo.com>
2016-07-06 10:51:41 -0700

    7/6/16

--- src/com/plantronics/com/scrape/sfdc/KbArticle.java
+++ src/com/plantronics/com/scrape/sfdc/KbArticle.java
@@ -25,8 +25,7 @@ public class KbArticle {
 	private String searchTitle;
 	private String articleType;
 	private String familyType;
-	private String keywordSearch;
-	private String submit; 
+	private String keywordSearch; 
 	private Document page;
 	private String[] children;
 	private static final Logger log = Logger.getLogger(KbArticle.class);
@@ -50,7 +49,6 @@ public class KbArticle {
     	articleType = null;
     	familyType = null;
     	keywordSearch = null;
-    	submit = null;
     	page = null;
     	children = null;
     }
@@ -60,221 +58,326 @@ public class KbArticle {
     	url = value;
     	try {
 			page = Jsoup.connect(url).get();
+			log.info("URL: " + url);
 			
-		} catch (Exception e) {
-			log.fatal("could not make connection with URL, error message: " + e);
+		} 
+    	catch (Exception e) {
+			log.fatal("URL connection fail, error message: " + e);
 			e.printStackTrace();
 		}
     }
+    
     public String[] getChildren(){
 
-      Elements child;
       try{
 	  	  Element div = page.select("div.blockDetail").first();
-	  	  child = div.children();
+	  	  Elements child = div.children();
 	  	  children = new String[child.size()];
 	  	  for(int i = 0; i < child.size(); i++){
 	 		  children[i] = (child.get(i).attr("class"));
 	 	  }
-      }catch(Exception e){
-    	  log.fatal("could not make connection with URL, error message: " + e);
+	  	  log.info("Number of child DIV: " + children.length);
+      }
+      catch(Exception e){
+    	  log.fatal("No child DIV found , error message: " + e);
     	  e.printStackTrace();
       }
   
   	  return(children);
     }
     
-    public String getUrl()
-    {
+    public String getUrl(){
+    	
     	return(url);
     }
     
     public String getId(){
-
-    	for (Element searchResult : page.select("div.blockMetaData")) {
-    		id = searchResult.select("span").text();
-        }
+    	
+    	try{
+	    	for (Element searchResult : page.select("div.blockMetaData")) {
+	    		id = searchResult.select("span").text();
+	        }
+	    	log.info("Article ID: " + id);
+    	}
+    	catch(Exception e){
+    		log.error("No ID found, error message: " + e);
+      	  	e.printStackTrace();
+    	}
+    	
     	return(id);
     }
     
     public String getTitle(){
-
-    	for(Element searchResult : page.select("div.blockDetail")) {
-    		Element div = searchResult.child(1);   
-    		title = div.select("h3").text();
-	    }
-    	log.info("Title: " + title);
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockDetail")) {
+	    		Element div = searchResult.child(1);   
+	    		title = div.select("h3").text();
+		    }
+	    	log.info("Title: " + title);
+    	}
+    	catch(Exception e){
+    		log.error("No Title found, error message: " + e);
+      	  	e.printStackTrace();
+    	}
+    	
     	return(title);
     }
     
     public String getTitle2(){
-
-    	for(Element searchResult : page.select("div.blockDetail")) {
-    		Element div = searchResult.child(3);   
-			title2 = div.select("h3").text();
-	    }
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockDetail")) {
+	    		Element div = searchResult.child(3);   
+				title2 = div.select("h3").text();
+		    }
+	    	log.info("Content title: " + title2);
+    	}
+    	catch(Exception e){
+    		log.error("No Content Title found, error message: " + e);
+      	  	e.printStackTrace();
+    	}
+    	
     	return(title2);
-   }
+    }
     
     public String getContent(){
-
-    	for(Element searchResult : page.select("div.blockDetail")) {
-    		Element div = searchResult.child(2);
-    		content = div.html();
-      	}
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockDetail")) {
+	    		Element div = searchResult.child(2);
+	    		content = div.html();
+	      	}
+	    	log.info("Question/Content: " + content);
+    	}
+    	catch(Exception e){
+    		log.error("No Question/Content found, error message: " + e);
+      	  	e.printStackTrace();
+    	}
+    	
     	content = cleanHtml(content);
     	return(content);
     }
     
    public String getContent2(){
-
-	   for (Element searchResult : page.select("div.blockDetail")) {
-    		Element div = searchResult.child(4);
-    		content2 = div.html();
+	   
+	   try{
+		   for (Element searchResult : page.select("div.blockDetail")) {
+	    		Element div = searchResult.child(4);
+	    		content2 = div.html();
+		   }
+		   log.info("Content: " + content2);
 	   }
+	   catch(Exception e){
+		   log.error("No Content found, error message: " + e);
+     	   e.printStackTrace();
+	   }
+	   
     	content2 = cleanHtml(content2);
     	return(content2);
     }
         
     public String getSearchResults(){
-  
-    	page.select("div.blockPagination").remove();
-    	page.select("div.blockResults h3").remove();
-  	    
-    	for (Element searchResult : page.select("div.blockResults")) {
-  	    	searchResults = searchResult.html();
-	    }
+    	
+    	try{
+	    	page.select("div.blockPagination").remove();
+	    	page.select("div.blockResults h3").remove();
+	  	    
+	    	for (Element searchResult : page.select("div.blockResults")) {
+	  	    	searchResults = searchResult.html();
+		    }
+	    	log.info("Search Results: " + searchResults);
+    	}
+    	catch(Exception e){
+    		log.error("No Search Results found, error message: " + e);
+      	    e.printStackTrace();
+    	}
+    	
     	searchResults = cleanHtml(searchResults);
     	return(searchResults);
     }
     
-  public String getPdf(){
-
-	   for(Element searchResult : page.select("div.blockDetail")) {
-		   pdf = searchResult.select("a").outerHtml();
-       }
-	   pdf = cleanHtml(pdf);
-       return(pdf);
+    public String getPdf(){
+    	try{
+    		for(Element searchResult : page.select("div.blockDetail")) {
+    			pdf = searchResult.select("a").outerHtml();
+    		}
+    		log.info("PDF: " + pdf);
+  	  	}catch(Exception e){
+  		   log.error("No PDF found, error message: " + e);
+  		   e.printStackTrace();
+  	  	}
+	  
+    	pdf = cleanHtml(pdf);
+    	return(pdf);
     }
    
-   public String getQuestionTitle(){
-
-	   for(Element searchResult : page.select("div.blockDetail")) {
-  		   Element div = searchResult.child(2);   
-  		   questTitle = div.select("h4").text();
-	   }
-	   return(questTitle);
-    }
+    public String getQuestionTitle(){
+    	try{
+    		for(Element searchResult : page.select("div.blockDetail")) {
+    			Element div = searchResult.child(2);   
+	  		    questTitle = div.select("h4").text();
+		    }
+		    log.info("Question info: " + questTitle);
+	    }
+	    catch(Exception e){
+		    log.error("No Question Title found, error message: " + e);
+     	    e.printStackTrace();
+	    }
+	   
+	    return(questTitle);
+     }
    
    	public String getAnswerTitle(){
-  
-   		for(Element searchResult : page.select("div.blockDetail")) {
-	  		Element div = searchResult.child(4);   
-	  		answerTitle = div.select("h4").text();
-		}
+   		try{
+	   		for(Element searchResult : page.select("div.blockDetail")) {
+		  		Element div = searchResult.child(4);   
+		  		answerTitle = div.select("h4").text();
+			}
+	   		log.info("Answer Title: " + answerTitle);
+   		}
+   		catch(Exception e){
+   			log.error("No Answer Title found, error message: " + e);
+      	    e.printStackTrace();
+   		}
+   		
 	  	return(answerTitle);
 	  }
    	
    	public String getQuestionContent(){
-
-   		for(Element searchResult : page.select("div.blockDetail")) {
-   			Element div = searchResult.child(3);
-   			questionContent = div.html();
-     	}
+   		
+   		try{
+	   		for(Element searchResult : page.select("div.blockDetail")) {
+	   			Element div = searchResult.child(3);
+	   			questionContent = div.html();
+	     	}
+	   		log.info("Question Info: " + questionContent);
+   		}
+   		catch(Exception e){
+   			log.error("No Question Content found, error message: " + e);
+      	    e.printStackTrace();
+   		}
+   		
    		questionContent = cleanHtml(questionContent);
    		return(questionContent);
    	}
    
    	public String getAnswerContent(){
-
-   		for(Element searchResult : page.select("div.blockDetail")) {
-   			Element div = searchResult.child(5);
-   			answerContent = div.html();
-     	}
+   		try{
+	   		for(Element searchResult : page.select("div.blockDetail")) {
+	   			Element div = searchResult.child(5);
+	   			answerContent = div.html();
+	     	}
+	   		log.info("Answer Content: " + answerContent);
+   		}
+   		catch(Exception e){
+   			log.error("No Answer Content found, error message: " + e);
+      	    e.printStackTrace();
+   		}
+   		
    		answerContent = cleanHtml(answerContent);
    		return(answerContent);
    	}
    
-   public String cleanHtml(String html){
-	   
-	   Document doc = Jsoup.parse(html);
-	   doc.select("font").unwrap();
+    public String cleanHtml(String html){
+   		Document doc = Jsoup.parse(html);
+	    doc.select("font").unwrap();
 
-	   NodeTraversor traversor  = new NodeTraversor(new NodeVisitor() {
+	    NodeTraversor traversor  = new NodeTraversor(new NodeVisitor() {
 		   
-		   public void tail(Node node, int depth) {
-			   if (node instanceof Element) {
+	    	public void tail(Node node, int depth) {
+	    		if (node instanceof Element) {
 				   Element e = (Element) node;
 				   e.removeAttr("class");
 				   e.removeAttr("style");
-			   }
-		   }
+			    }
+		    }
 
-		   public void head(Node node, int depth) {        
-		   }
-	   });
-
-	   traversor.traverse(doc.body());
-	   String modifiedHtml = doc.toString();
-	   modifiedHtml = modifiedHtml.replaceAll("<html>", "");
-	   modifiedHtml = modifiedHtml.replaceAll("</html>", "");
-	   modifiedHtml = modifiedHtml.replaceAll("<body>", "");
-	   modifiedHtml = modifiedHtml.replaceAll("</body>", "");
-	   modifiedHtml = modifiedHtml.replaceAll("<head>", "");
-	   modifiedHtml = modifiedHtml.replaceAll("</head>", "");
+		    public void head(Node node, int depth) {        
+		    }
+	    });
+	    
+	    traversor.traverse(doc.body());
+	    String modifiedHtml = doc.toString();
+	    modifiedHtml = modifiedHtml.replaceAll("<html>", "");
+	    modifiedHtml = modifiedHtml.replaceAll("</html>", "");
+	    modifiedHtml = modifiedHtml.replaceAll("<body>", "");
+	    modifiedHtml = modifiedHtml.replaceAll("</body>", "");
+	    modifiedHtml = modifiedHtml.replaceAll("<head>", "");
+	    modifiedHtml = modifiedHtml.replaceAll("</head>", "");
 	
-	   return (modifiedHtml);
-   	}
+	    return (modifiedHtml);
+    }
    
-   public String getSearchTitle(){
-
- 		for(Element searchResult : page.select("div.blockSearch fieldset")) {
- 			Element div = searchResult.child(0);
- 			searchTitle = div.text();
- 		}
+    public String getSearchTitle(){
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockSearch fieldset")) {
+	 			Element div = searchResult.child(0);
+	 			searchTitle = div.text();
+	 		}
+	    	log.info("Search Title: " + searchTitle);
+    	}
+    	catch(Exception e){
+    		log.error("No Search Title found, error message: " + e);
+      	    e.printStackTrace();
+    	}
+    	
  		searchTitle = cleanHtml(searchTitle);
  		return(searchTitle);
  	}
    
-   public String getArticleType(){
-
-  		for(Element searchResult : page.select("div.blockSearch fieldset")) {
- 			Element div = searchResult.child(1);
-   			articleType = div.html();
-     	}
+    public String getArticleType(){
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockSearch fieldset")) {
+	 			Element div = searchResult.child(1);
+	   			articleType = div.html();
+	     	}
+	    	log.info("Article Type: " + articleType);
+    	}
+    	catch(Exception e){
+    		log.error("No Article Type found, error message: " + e);
+      	    e.printStackTrace();
+    	}
+    	
    		articleType = cleanHtml(articleType);
   		return(articleType);
   	}
    
-   public String getFamilyType(){
-
- 		for(Element searchResult : page.select("div.blockSearch fieldset")) {
-			Element div = searchResult.child(2);
-  			familyType = div.html();
+    public String getFamilyType(){
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockSearch fieldset")) {
+				Element div = searchResult.child(2);
+	  			familyType = div.html();
+	    	}
+	    	log.info("Family Type: " + familyType);
     	}
+    	catch(Exception e){
+    		log.error("No Family Type found, error message: " + e);
+      	    e.printStackTrace();
+    	}
+    	
   		familyType = cleanHtml(familyType);
  		return(familyType);
  	}
    
-   public String getkeywordSearch(){
-
- 		for(Element searchResult : page.select("div.blockSearch fieldset")) {
-			Element div = searchResult.child(3);
-  			keywordSearch = div.html();
+    public String getkeywordSearch(){
+    	
+    	try{
+	    	for(Element searchResult : page.select("div.blockSearch fieldset")) {
+				Element div = searchResult.child(3);
+	  			keywordSearch = div.html();
+	    	}
+	    	log.info("Keyword Search: " + keywordSearch);
     	}
+    	catch(Exception e){
+    		log.error("No Keyword Search found, error message: " + e);
+      	    e.printStackTrace();
+    	}
+    	
  		keywordSearch = cleanHtml(keywordSearch);
  		return(keywordSearch);
  	}
-   
-   public String getSubmit(){
-
-		for(Element searchResult : page.select("div.blockSearch fieldset")) {
-			Element div = searchResult.child(4);
- 			submit = div.html();
-   	}
-		submit = cleanHtml(submit);
-		return(submit);
-	}
-   
-
 }
